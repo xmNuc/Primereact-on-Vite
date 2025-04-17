3b2cf1cf1aff594ebb89dbb75c25ea54de31fc15
xmNuc <xmnuc@o2.pl>
2025-04-17 09:44:02 +0200

    update formatter

--- src/utils/timeDateFormatter.ts
+++ src/utils/timeDateFormatter.ts
@@ -1,11 +1,11 @@
 export const formatTimeString = (timeUtc: string, date: string) => {
   const userLocale = navigator.language;
   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const [timeHH, timeMM, timeSS = '0'] = timeUtc.split(':').map(Number);
+  const [timeHH, timeMM] = timeUtc.split(':').map(Number);
 
   const [year, month, day] = date.split('-').map(Number);
 
-  const objDate = new Date(Date.UTC(year, month - 1, day, timeHH, timeMM, timeSS));
+  const objDate = new Date(Date.UTC(year, month - 1, day, timeHH, timeMM));
 
   const timeOptions: Intl.DateTimeFormatOptions = {
     timeZone,
