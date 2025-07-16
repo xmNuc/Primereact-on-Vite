c9a79f242a9d62ce7cfa39fd762eb8b1d2d9af8d
xmNuc <xmnuc@o2.pl>
2025-07-16 08:57:57 +0200

    added getExcelNumberFormatByLocale v2

---
+++ src/components/exportToExcelV2/getExcelNumberFormatByLocale2.ts
@@ -0,0 +1,28 @@
+export const getExcelNumberFormatByLocale = (
+  locale: string,
+  maxFractionDigits: number = 5
+): string => {
+  const testNumber = 1234567.89;
+
+  const formatted = new Intl.NumberFormat(locale, {
+    maximumFractionDigits: maxFractionDigits,
+  }).format(testNumber);
+
+  const decimalMatch = formatted.match(/(\D)\d{2}$/);
+  const decimalSeparator = decimalMatch ? decimalMatch[1] : '.';
+
+  const thousandsMatch = formatted.match(/(\D)\d{3}\D?\d{2}$/);
+  const thousandsSeparator =
+    thousandsMatch && thousandsMatch[1] !== decimalSeparator ? thousandsMatch[1] : '';
+
+  const hasDecimalPart = maxFractionDigits > 0;
+  const decimalPart = hasDecimalPart ? `${decimalSeparator}${'#'.repeat(maxFractionDigits)}` : '';
+
+  let numFmt = `#${thousandsSeparator}##0${decimalPart}`;
+
+  if (hasDecimalPart && numFmt.endsWith(decimalSeparator)) {
+    numFmt = numFmt.slice(0, -1);
+  }
+
+  return numFmt;
+};
