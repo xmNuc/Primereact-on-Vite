9f6c551d36b0e44a3fdf235e3d4fc87cba11aec8
xmNuc <xmnuc@o2.pl>
2026-06-16 15:59:39 +0200

    potential update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
+
+// export default defineConfig({
+//   plugins: [
+//     react({
+//       babel: {
+//         plugins: ['babel-plugin-styled-components'],
+//       },
+//     }),
+//   ],
+
+//   resolve: {
+//     alias: {
+//       'styled-components': path.resolve(
+//         __dirname,
+//         'node_modules/styled-components'
+//       ),
+//     },
+//   },
+
+//   define: {
+//     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+//     'process.env': process.env,
+//   },
+
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         manualChunks: undefined,
+//       },
+//     },
+//   },
+// });
