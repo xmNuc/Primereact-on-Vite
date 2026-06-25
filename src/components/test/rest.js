a7986ac2b9c61ebb650ec131311a7275864213d3
xmNuc <xmnuc@o2.pl>
2026-06-25 17:03:00 +0200

    add formik component

---
+++ src/components/test/rest.js
@@ -0,0 +1,10 @@
+<Field
+  name="formik component"
+  component={SelectField}
+  options={[
+    { value: 'LOW', label: 'LOW' },
+    { value: 'MEDIUM', label: 'MEDIUM' },
+    { value: 'HIGH', label: 'HIGH' },
+  ]}
+  isClearable
+/>;
